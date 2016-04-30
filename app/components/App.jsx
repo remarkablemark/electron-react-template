b198d913431c9eae25b467f537331977ff9b56ac
Mark <mark@remarkablemark.org>
2016-04-29 23:38:16 -0400

    Setup basic React app

---
+++ app/components/App.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var React = require('react');
+
+/**
+ * App component.
+ */
+module.exports = React.createClass({
+    render: function() {
+        return (
+            <h1>
+                Welcome to electron-react-template!
+            </h1>
+        );
+    }
+});
---
+++ app/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var React = require('react');
+var ReactDOM = require('react-dom');
+var App = require('./components/App');
+
+ReactDOM.render(<App />, document.getElementById('app'));
