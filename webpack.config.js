e3c2198a8e917407c2749732886d82e6acf4321a
Mark <mark@remarkablemark.org>
2016-04-29 23:31:37 -0400

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var path = require('path');
+
+/**
+ * Export webpack config.
+ */
+module.exports = {
+    entry: path.join(__dirname, '/app/main.js'),
+
+    output: {
+        path: path.join(__dirname, '/app/build/'),
+        filename: '[name].js'
+    },
+
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel', // 'babel-loader' is also a legal name to reference
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ]
+    },
+
+    resolve: {
+        extensions: ['', '.js', '.jsx']
+    },
+
+    devtool: 'eval-source-map'
+};
